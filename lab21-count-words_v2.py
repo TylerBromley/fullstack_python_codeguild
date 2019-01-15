2717268741ff0be069ceb8b13528bf6ee6339747
Tyler Bromley <thelony@gmail.com>
2019-01-15 15:24:11 -0800

    lab21 removed stopwords from v2

--- lab21-count-words_v2.py
+++ lab21-count-words_v2.py
@@ -2,18 +2,19 @@
 
 import string
 
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
 translator = str.maketrans("", "", string.punctuation)
 with open('a-modest-proposal.txt') as f:
 	word_list = f.read().lower().translate(translator).split()
 
 def get_bigrams(word_list):
 	bigram_list = [(word_list[i], word_list[i + 1]) for i in range(len(word_list)-1)]
-	# bigram_list = []
-	# for i in range(len(word_list)-1):
-	# 	bigram_list.append((word_list[i], word_list[i + 1]))
+	bigram_list = [i for i in bigram_list if i[0] not in stopwords and i[1] not in stopwords]
 	bigram_count = [bigram_list.count(i) for i in bigram_list]
-	bigram_dict = dict(zip(bigram_list, bigram_count))
+	bigram_dict = dict(zip(bigram_list, bigram_count))			
 	bigrams = list(bigram_dict.items())
+	# print(bigrams)
 	bigrams.sort(key=lambda tup: tup[1], reverse=True)
 	return [bigrams[i] for i in range(min(10, len(bigrams)))]
 
